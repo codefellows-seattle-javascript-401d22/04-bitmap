36e9282668cf4769440c0acf6f590fe1c47defa5
Bessie Arino <brarino@gmail.com>
2018-02-16 14:52:11 -0800

    fixed linter issues

--- lab-alex-bessie-david/index.js
+++ lab-alex-bessie-david/index.js
@@ -5,11 +5,11 @@ const transformer = require(`${__dirname}/lib/transformer.js`);
 const writer = require(`${__dirname}/lib/writer.js`);
 
 // node index.js './data/palette-bitmap.bmp' './data/newpalettebitmap.bmp' 'blackout'
-reader(process.argv[2], function(err,data){
-    let buff = transformer(data, process.argv[4]);
+reader(process.argv[2], function (err, data) {
+  let buff = transformer(data, process.argv[4]);
 
-    writer(process.argv[3], buff, function(err,data){
-        console.log('done!');
-    });
+  writer(process.argv[3], buff, function (err, data) {
+    console.log('done!');
+  });
 
 });
