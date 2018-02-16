2b91757008980f80bdfce6170a6556b7bc369aa7
Melanie Cohen <melaniebcohen@gmail.com>
2018-02-16 15:15:08 -0800

    colors are changing

--- lab-taylor-melanie-ken/lib/bitmap-writer.js
+++ lab-taylor-melanie-ken/lib/bitmap-writer.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const fs = require('fs');
-//const reader = require('./bitmap-reader.js');
-//const constructor = require('./bitmap-constructor.js');
+const reader = require('./bitmap-reader.js');
+const constructor = require('./bitmap-constructor.js');
 
 const bitMapWriter = module.exports = function(file, newBM, callback) {
     
@@ -13,12 +13,21 @@ const bitMapWriter = module.exports = function(file, newBM, callback) {
 
         //console.log(data);
         let fileBuffer = data;
-        console.log(fileBuffer);
+        // console.log(fileBuffer);
         
+        let newBitMap = constructor(data);
+        // console.log(newBitMap);
+        console.log(newBitMap.colorTable);
+
+        for (let i = 0; i < newBitMap.colorTable.length; i++) {
+            if (newBitMap.colorTable[i] !== 0) {
+                newBitMap.colorTable[i] = 39;
+            }
+        }
+        console.log(newBitMap.colorTable);
+
         fs.writeFile(newPath, fileBuffer, function(err,data){
             if(err) throw err;
-            
-    
         });
     });
 }
