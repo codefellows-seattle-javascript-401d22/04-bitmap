821fda417c3305cf51d73fdd041304f0503e0621
Bessie Arino <brarino@gmail.com>
2018-02-16 08:58:46 -0800

    fixed tabs

--- lab-alex-bessie-david/lib/writer.js
+++ lab-alex-bessie-david/lib/writer.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 
-const writer = module.exports = function(path, buff, callback){
-    fs.writeFile(path, buff, function(err,data){
-        if(err) return callback(err);
-        return callback(null, data);
-    });
+const writer = module.exports = function (path, buff, callback) {
+  fs.writeFile(path, buff, function (err, data) {
+    if (err) return callback(err);
+    return callback(null, data);
+  });
 }
