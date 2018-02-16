b5d9fb9d3c944a0fcfa8c8e58d527b04e30b08d7
Your Name <nords3x4@gmail.com>
2018-02-15 16:04:37 -0800

    read and write file written

--- lab-greg-nicole-ahmed/lib/read-write-file.js
+++ lab-greg-nicole-ahmed/lib/read-write-file.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const constructor = require('../lib/constructor.js');
+const fs = require('fs');
+
+
+const readFileHelper = module.exports = function(file, newFile, callback) {
+  let filePathRead = `${__dirname}/${file}`;
+  let filePathWrite = `${__dirname}/${newFile}`;
+  fs.readFile(filePathRead, function(err, data){
+    // if(err)
+    let bitMap = constructor(data);
+    //let newBitmap = allblack(bitMap.pixelArray);
+    fs.writeFile(filePathWrite, newBitmap, function(err, data){
+      // if(err)
+      console.log('file successfully created');
+    });
+  });
+  
+
+};
