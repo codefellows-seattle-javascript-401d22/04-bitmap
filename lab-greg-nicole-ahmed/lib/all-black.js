8d44621fe0615f287169baeabeffc027e14dbc1f
nicoleweese <nweese@ymail.com>
2018-02-15 17:53:47 -0800

    turned picture white!!!!

--- lab-greg-nicole-ahmed/lib/all-black.js
+++ lab-greg-nicole-ahmed/lib/all-black.js
@@ -1,1 +1,21 @@
 'use strict';
+
+module.exports = function(array) {
+  let colorString = '';
+  let colorPalette = '';
+  let numOfZeros = array.length * 8;
+
+
+
+  for(var i = 0; i < numOfZeros; i++) {
+    colorString += 'f';
+  }
+
+  for(var j = 0; j < 1024; j++) {
+    colorPalette += 'f';
+  }
+
+  // console.log('black string', blackString);
+
+  return {colorString: colorString, colorPalette: colorPalette};
+}
--- lab-greg-nicole-ahmed/lib/constructor.js
+++ lab-greg-nicole-ahmed/lib/constructor.js
@@ -1,14 +1,19 @@
 'use strict';
 
-module.export = function(data) {
+module.exports = function(data) {
   function Bitmap (data) {
     this.size = data.readInt32LE(2);
     this.width = data.readInt32LE(18);
     this.height = data.readInt32LE(20);
     this.pixelAddress = data.readInt32LE(10);
     this.pixels = data.toString('hex', this.pixelAddress, (this.pixelAddress + this.size));
+
+    
+    this.header = data.slice(0, 54).toString('hex');
+    this.colorPalette = data.slice(54, this.pixelAddress).toString('hex');
     this.pixelArray = this.pixels.match((/.{1,8}/g));
-    this.header = data.slice(0, this.pixelAddress).toString('hex');
     this.end = data.slice(this.pixelAddress + this.size, data.length).toString('hex');
   }
+
+  return new Bitmap(data);
 }
