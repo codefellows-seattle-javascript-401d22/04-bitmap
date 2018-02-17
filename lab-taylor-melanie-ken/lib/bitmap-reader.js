963b2b4f1e596bc2aed83098433fed5c2bfe0c94
Melanie Cohen <melaniebcohen@gmail.com>
2018-02-16 17:58:58 -0800

    included linter and test structure

--- lab-taylor-melanie-ken/lib/bitmap-reader.js
+++ lab-taylor-melanie-ken/lib/bitmap-reader.js
@@ -1,17 +1,15 @@
 'use strict';
 
-
 const fs = require('fs');
 
-
 const bitMapReader = module.exports = function(file, callback) {
-    fs.readFile(file, function(err,data) {
-        if(err) return callback(err);
+  fs.readFile(file, function(err,data) {
+    if(err) return callback(err);
 
-        //console.log(data);
-        return data;
-    })
-}
+    //console.log(data);
+    return data;
+  });
+};
     
 
 
--- lab-taylor-melanie-ken/lib/transform.js
+++ lab-taylor-melanie-ken/lib/transform.js
@@ -5,7 +5,7 @@ const writer = require('./bitmap-writer.js');
 const constructor = require('./bitmap-constructor.js');
 
 const bitMapTransform = module.exports = function(data, callback) {
-  console.log('Color table data transform:',data)
+  console.log('Color table data transform:', data);
 
   for (let i = 0; i < data.length; i++) {
     if (data[i] === 0) {
@@ -13,9 +13,9 @@ const bitMapTransform = module.exports = function(data, callback) {
     }
   }
 
-  console.log('Color table data post-transform:',data)
+  console.log('Color table data post-transform:',data);
   return data;
-}
+};
 
     
     
