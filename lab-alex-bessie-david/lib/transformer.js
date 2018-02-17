38546b5468f6d665597b040541b892e07d45b770
DKosmos <davidakosmos@gmail.com>
2018-02-16 16:52:54 -0800

    got invert working

--- lab-alex-bessie-david/lib/transformer.js
+++ lab-alex-bessie-david/lib/transformer.js
@@ -3,7 +3,7 @@
 const transformer = module.exports = function(buff, transformType){
     switch(transformType){
         case 'blackout':
-            var start = buff.readInt32LE(14) + 14;
+            var start = buff.readInt32LE(14) + 15;
             var offset = buff.readInt32LE(10);
             for (let i = start; i < offset; i++){
                 buff[i] = 0;
@@ -15,14 +15,17 @@ const transformer = module.exports = function(buff, transformType){
             console.log('start:', start);
             var end = start + buff.readInt32LE(34);
             console.log('end:', end);
-            var pixels = buff.toString('hex', start, end).match(/.{8}/g).reverse().join('').match(/.{2}/g).join('');
-            console.log('pixels:', pixels);
+            console.log('original buff string length: ', buff.toString().length);
+            var pixels = buff.toString('hex', start, end).match(/.{8}/g).reverse().join('').match(/.{2}/g);
+            console.log('pixels:', pixels.length);
             buff = buff.toString('hex').match(/.{2}/g);
-            console.log('original buff:', buff);
+            console.log('original buff:', buff.length);
             buff.splice(start, (end - start + 1));
-            console.log('second buff:', buff);
-            buff = buff.toString().replace(/[,]/g, '').concat(pixels);;
+            buff = buff.concat(pixels);
+            console.log('second buff:', buff.length);
+            buff = buff.toString().replace(/[,]/g, '');
             buff = Buffer.from(buff, 'hex');
             console.log('buffend', buff);
+            return buff;
     }
 }
