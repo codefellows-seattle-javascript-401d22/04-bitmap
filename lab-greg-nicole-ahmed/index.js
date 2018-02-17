c6bb5f90eea1818eafa05f25cbc7846d7ce56560
nicoleweese <nweese@ymail.com>
2018-02-16 18:30:13 -0800

    darkening of colors finished

--- lab-greg-nicole-ahmed/index.js
+++ lab-greg-nicole-ahmed/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+
 const fileReader = require('./lib/read-write-file.js');
 const allwhite = require('./lib/all-black.js');
 const blueStripe = require('./lib/blue-stripe.js');
+const darken = require('./lib/darken.js');
 
-
-fileReader('palette-bitmap.bmp', 'new.bmp', allwhite);
-fileReader('palette-bitmap.bmp', 'blueStripe.bmp',blueStripe);
+// fileReader('palette-bitmap.bmp', 'new.bmp', allwhite);
+// fileReader('palette-bitmap.bmp', 'blueStripe.bmp',blueStripe);
+fileReader('palette-bitmap.bmp', 'darkened.bmp', darken);
--- lab-greg-nicole-ahmed/lib/read-write-file.js
+++ lab-greg-nicole-ahmed/lib/read-write-file.js
@@ -1,34 +1,23 @@
 'use strict';
 
 const constructor = require('./constructor.js');
-const allwhite = require('./all-black.js');
 const fs = require('fs');
 const path = require('path');
 
 
 const readFileHelper = module.exports = function(file, newFile, callback) {
   let filePathRead = path.join(__dirname, '..', 'data', file);
-
-  // console.log('path to read from', filePathRead);
-  
-  // console.log('file path we sent', `${file}`);
   let filePathWrite = `${__dirname}/../data/${newFile}`;
 
   fs.readFile(filePathRead, function(err, data){
-    // if(err)
-    // console.log('this is our file from the fileRead', data);
+    
     let bitMap = constructor(data);
-    // console.log('this is our callback', callback);
     let newBitmapObj = callback(bitMap);
-    // RECEIVES STRINGS!!! v v v 
+    // the transformation for the callback must return strings in order to run
     let newBitmapTotal = bitMap.header + newBitmapObj.colorPalette + newBitmapObj.colorString + bitMap.end;
     let newBitmap = Buffer.from(newBitmapTotal, 'hex');
-    // console.log(newBitmap.toString('hex'));
-    // console.log('new bitmap buffer', newBitmap);
     fs.writeFile(filePathWrite, newBitmap, 'hex', function(err, data){
-      // if(err)
-      // console.log('file successfully created');
-
+      console.log('file written successfully! go check out your pretty picture!');
     });
   });
   
