d0cbe2d567751fc135020066319164db19cc77df
DKosmos <davidakosmos@gmail.com>
2018-02-16 18:17:04 -0800

    grayscale working

--- lab-alex-bessie-david/lib/transformer.js
+++ lab-alex-bessie-david/lib/transformer.js
@@ -3,7 +3,7 @@
 const transformer = module.exports = function(buff, transformType){
     switch(transformType){
         case 'blackout':
-            var start = buff.readInt32LE(14) + 15;
+            var start = buff.readInt32LE(14) + 14;
             var offset = buff.readInt32LE(10);
             for (let i = start; i < offset; i++){
                 buff[i] = 0;
@@ -26,6 +26,34 @@ const transformer = module.exports = function(buff, transformType){
             buff = buff.toString().replace(/[,]/g, '');
             buff = Buffer.from(buff, 'hex');
             console.log('buffend', buff);
+            return buff;
+        case 'grayscale':
+            var start = buff.readInt32LE(14) + 14;
+            var offset = buff.readInt32LE(10);
+            var colors = buff.toString('hex', start, offset).match(/.{8}/g);
+            console.log('beforecolors', colors.length);
+            // Looking at the below stack overflow helped me figure out hex to decimal
+            // https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
+            // The website below is where I got the math for grayscale conversion
+            // https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/
+            var graycolors = colors.map(color => {
+                var blue = '0x' + color.slice(0,2);
+                var green = '0x' + color.slice(2,4);
+                var red = '0x' + color.slice(4,6);
+
+                var gray = Math.floor((+red * 0.21) + (+green * 0.72) + (+blue * 0.07));
+                gray = gray.toString(16);
+                if(gray.length === 1){ gray = '0' + gray};
+                var ans = gray + gray + gray + '01';
+                return ans;
+            });
+            console.log('aftercolors:', graycolors);
+            graycolors = graycolors.join('');
+            console.log('graycolors.length:', graycolors.length);
+            buff = buff.toString('hex', 0, start) + graycolors + buff.toString('hex', offset, buff.readInt32LE(2));
+            console.log(buff.length);
+            buff = Buffer.from(buff, 'hex');
+            console.log(buff.length);
             return buff;
     }
 }
