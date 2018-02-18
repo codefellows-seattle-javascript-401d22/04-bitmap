55269562ab29413ef56d779ec0ce21d1ce10ff9b
nicoleweese <nweese@ymail.com>
2018-02-18 11:52:40 -0800

    darkened feature done

--- lab-greg-nicole-ahmed/index.js
+++ lab-greg-nicole-ahmed/index.js
@@ -5,7 +5,9 @@ const fileReader = require('./lib/read-write-file.js');
 const allwhite = require('./lib/all-black.js');
 const blueStripe = require('./lib/blue-stripe.js');
 const darken = require('./lib/darken.js');
+const lighten = require('./lib/lighten.js');
 
 // fileReader('palette-bitmap.bmp', 'new.bmp', allwhite);
 // fileReader('palette-bitmap.bmp', 'blueStripe.bmp',blueStripe);
 fileReader('palette-bitmap.bmp', 'darkened.bmp', darken);
+fileReader('palette-bitmap.bmp', 'lightened.bmp', lighten);
